fc669fdcdde64c22364f5565e9f901ee523e95b7
akabawi <akabawi@gmail.com>
2019-01-12 13:51:06 +0400

    .

--- fin.py
+++ fin.py
@@ -8,7 +8,7 @@ def total_year(d):
 	for i in d.iterrows():
 		a.append(i[1].iloc[4])
 
-	return sum(a)
+	return int(sum(a))
 
 def total_month(d,m):
 
@@ -16,10 +16,26 @@ def total_month(d,m):
 	for i in d.iterrows():
 		if i[1].iloc[1] == m:
 			a.append(i[1].iloc[4])
-	return sum(a)
+	return int(sum(a))
+
+def display_cat(d):
+
+	s=set()
+	for i in d.iterrows():
+		if i[1].iloc[3] not in s:
+			s.add(i[1].iloc[3])
+	print("\nCategories:\n")
+	for i in s:
+		tm=0
+		for n in d.iterrows():
+			if n[1].iloc[3] == i:
+				tm = tm + int(n[1].iloc[4])
+		print (i + ": " + str(tm))		
 
 
-os.chdir("/home/akabawi/Documents/Mobills/")
+
+
+os.chdir("/home/akabawi/pCloudDrive/Finances/")
 
 file='Expenses 2018.xlsx'
 
@@ -33,4 +49,6 @@ for i in range(2,13):
 	print(str(i) + ": " + str(total_month(df,i)))
 
 print("\nTotal for the year: AED " + str(total_year(df)))
-print("\nAverage Monthly Spend: AED " + str(total_year(df)/11))
+print("\nAverage Monthly Spend: AED " + str(int(total_year(df)/11)) + "\n")
+
+display_cat(df)
